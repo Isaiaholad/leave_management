5c7342eca1b265181456300b0ed03121c6512908
Isaiaholad <69095210+Isaiaholad@users.noreply.github.com>
2023-07-03 19:25:26 +0100

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,7 @@
+from LM_Window import Leavemanagement, StartApp
+
+# Create an instance of the Leavemanagement class
+app = Leavemanagement()
+
+# Start the application by creating an instance of the StartApp class
+start = StartApp(app)
